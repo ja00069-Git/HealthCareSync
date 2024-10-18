21fef510be2ad871069fcf44b985a559744db956
Md Mostafiur Rahman <mr00096@my.westga.edu>
2024-10-18 19:55:38 -0400

    Created a manage nurse view model class.

---
+++ HealthCareSync/ViewModels/ManageNursesViewModel.cs
@@ -0,0 +1,30 @@
+﻿using HealthCareSync.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareSync.ViewModels
+{
+    public class ManageNursesViewModel
+    {
+        /// <summary>
+        /// Gets the nurses.
+        /// </summary>
+        /// <value>
+        /// The nurses.
+        /// </value>
+        public BindingList<Nurse> nurses { get; }
+
+        public ManageNursesViewModel()
+        {
+            this.nurses = new BindingList<Nurse>{
+                new Nurse(1, "Sally", "Mae", new DateTime(2000, 10, 18), "614-456-1598", 0123, "ahmed"),
+                new Nurse(2, "Fatima", "Dalwai", DateTime.Now, null, null, "jabesi"),
+                new Nurse(2, "Mary", "Jane", DateTime.Now, null, null, "rahman")
+            };
+        }
+    }
+}
