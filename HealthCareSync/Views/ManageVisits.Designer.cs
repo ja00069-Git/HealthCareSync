a80e4272a1de9a49f524dfb1eb922f5415ab642c
Ahmad Hammett <ah00274@my.westga.edu>
2024-11-01 02:46:14 -0400

    Designed ManageVisit UI
    Designed ManageVisit UI for this current iteration, it features  a listbox of appointments(visits) and text boxes for the routine checkup

--- HealthCareSync/Views/ManageVisits.Designer.cs
+++ HealthCareSync/Views/ManageVisits.Designer.cs
@@ -29,26 +29,456 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            visitsListBox = new ListBox();
+            visitDateTimePicker = new DateTimePicker();
+            saveButton = new Button();
+            systolicTextBox = new TextBox();
+            bpmTextBox = new TextBox();
+            clearButton = new Button();
+            weightTextBox = new TextBox();
+            temperatureTextBox = new TextBox();
+            symptomsTextBox = new TextBox();
+            label2 = new Label();
+            appointmentIdTextBox = new TextBox();
+            diastolicTextBox = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            heightTextBox = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            panel15 = new Panel();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
+            panel11 = new Panel();
+            label14 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(282, 160);
+            label1.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold);
+            label1.ForeColor = Color.IndianRed;
+            label1.Location = new Point(96, 6);
             label1.Name = "label1";
-            label1.Size = new Size(189, 38);
-            label1.TabIndex = 0;
-            label1.Text = "Manage Visits";
+            label1.Size = new Size(65, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Visits";
+            // 
+            // visitsListBox
+            // 
+            visitsListBox.FormattingEnabled = true;
+            visitsListBox.ItemHeight = 15;
+            visitsListBox.Location = new Point(57, 39);
+            visitsListBox.Name = "visitsListBox";
+            visitsListBox.Size = new Size(139, 169);
+            visitsListBox.TabIndex = 2;
+            // 
+            // visitDateTimePicker
+            // 
+            visitDateTimePicker.CustomFormat = "MM/dd/yyyy   hh:mm tt";
+            visitDateTimePicker.Enabled = false;
+            visitDateTimePicker.Format = DateTimePickerFormat.Custom;
+            visitDateTimePicker.Location = new Point(301, 62);
+            visitDateTimePicker.Name = "visitDateTimePicker";
+            visitDateTimePicker.Size = new Size(154, 23);
+            visitDateTimePicker.TabIndex = 4;
+            // 
+            // saveButton
+            // 
+            saveButton.BackColor = Color.IndianRed;
+            saveButton.FlatStyle = FlatStyle.Flat;
+            saveButton.Font = new Font("Showcard Gothic", 9F);
+            saveButton.ForeColor = Color.White;
+            saveButton.Location = new Point(313, 246);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(75, 35);
+            saveButton.TabIndex = 5;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = false;
+            // 
+            // systolicTextBox
+            // 
+            systolicTextBox.BorderStyle = BorderStyle.None;
+            systolicTextBox.Font = new Font("Microsoft YaHei UI", 10F);
+            systolicTextBox.ForeColor = SystemColors.HotTrack;
+            systolicTextBox.Location = new Point(217, 128);
+            systolicTextBox.Name = "systolicTextBox";
+            systolicTextBox.Size = new Size(50, 17);
+            systolicTextBox.TabIndex = 6;
+            systolicTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // bpmTextBox
+            // 
+            bpmTextBox.BorderStyle = BorderStyle.None;
+            bpmTextBox.Font = new Font("Microsoft YaHei UI", 10F);
+            bpmTextBox.ForeColor = SystemColors.HotTrack;
+            bpmTextBox.Location = new Point(301, 178);
+            bpmTextBox.Name = "bpmTextBox";
+            bpmTextBox.Size = new Size(57, 17);
+            bpmTextBox.TabIndex = 8;
+            bpmTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // clearButton
+            // 
+            clearButton.BackColor = Color.IndianRed;
+            clearButton.FlatStyle = FlatStyle.Flat;
+            clearButton.Font = new Font("Showcard Gothic", 9F);
+            clearButton.ForeColor = Color.White;
+            clearButton.Location = new Point(217, 246);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(75, 35);
+            clearButton.TabIndex = 9;
+            clearButton.Text = "Clear";
+            clearButton.UseVisualStyleBackColor = false;
+            // 
+            // weightTextBox
+            // 
+            weightTextBox.BorderStyle = BorderStyle.None;
+            weightTextBox.Font = new Font("Microsoft YaHei UI", 10F);
+            weightTextBox.ForeColor = SystemColors.HotTrack;
+            weightTextBox.Location = new Point(396, 131);
+            weightTextBox.Name = "weightTextBox";
+            weightTextBox.Size = new Size(53, 17);
+            weightTextBox.TabIndex = 10;
+            weightTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // temperatureTextBox
+            // 
+            temperatureTextBox.BorderStyle = BorderStyle.None;
+            temperatureTextBox.Font = new Font("Microsoft YaHei UI", 10F);
+            temperatureTextBox.ForeColor = SystemColors.HotTrack;
+            temperatureTextBox.Location = new Point(298, 131);
+            temperatureTextBox.Name = "temperatureTextBox";
+            temperatureTextBox.Size = new Size(57, 17);
+            temperatureTextBox.TabIndex = 11;
+            temperatureTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // symptomsTextBox
+            // 
+            symptomsTextBox.BorderStyle = BorderStyle.None;
+            symptomsTextBox.Font = new Font("Microsoft YaHei UI", 10F);
+            symptomsTextBox.ForeColor = SystemColors.HotTrack;
+            symptomsTextBox.Location = new Point(412, 180);
+            symptomsTextBox.Multiline = true;
+            symptomsTextBox.Name = "symptomsTextBox";
+            symptomsTextBox.Size = new Size(125, 101);
+            symptomsTextBox.TabIndex = 12;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label2.Location = new Point(219, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 15);
+            label2.TabIndex = 13;
+            label2.Text = "Systolic";
+            // 
+            // appointmentIdTextBox
+            // 
+            appointmentIdTextBox.Enabled = false;
+            appointmentIdTextBox.Location = new Point(78, 240);
+            appointmentIdTextBox.Name = "appointmentIdTextBox";
+            appointmentIdTextBox.Size = new Size(100, 23);
+            appointmentIdTextBox.TabIndex = 14;
+            // 
+            // diastolicTextBox
+            // 
+            diastolicTextBox.BorderStyle = BorderStyle.None;
+            diastolicTextBox.Font = new Font("Microsoft YaHei UI", 10F);
+            diastolicTextBox.ForeColor = SystemColors.HotTrack;
+            diastolicTextBox.Location = new Point(217, 178);
+            diastolicTextBox.Name = "diastolicTextBox";
+            diastolicTextBox.Size = new Size(50, 17);
+            diastolicTextBox.TabIndex = 15;
+            diastolicTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label3.Location = new Point(217, 162);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 16;
+            label3.Text = "Diastolic";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label4.Location = new Point(291, 105);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 15);
+            label4.TabIndex = 17;
+            label4.Text = "Temperature";
+            // 
+            // heightTextBox
+            // 
+            heightTextBox.BorderStyle = BorderStyle.None;
+            heightTextBox.Font = new Font("Microsoft YaHei UI", 10F);
+            heightTextBox.ForeColor = SystemColors.HotTrack;
+            heightTextBox.Location = new Point(485, 131);
+            heightTextBox.Name = "heightTextBox";
+            heightTextBox.Size = new Size(52, 17);
+            heightTextBox.TabIndex = 18;
+            heightTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label5.Location = new Point(81, 223);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 15);
+            label5.TabIndex = 19;
+            label5.Text = "Appointment Id";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label6.Location = new Point(310, 163);
+            label6.Name = "label6";
+            label6.Size = new Size(32, 15);
+            label6.TabIndex = 20;
+            label6.Text = "BPM";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label7.Location = new Point(489, 105);
+            label7.Name = "label7";
+            label7.Size = new Size(43, 15);
+            label7.TabIndex = 21;
+            label7.Text = "Height";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label8.Location = new Point(441, 162);
+            label8.Name = "label8";
+            label8.Size = new Size(65, 15);
+            label8.TabIndex = 23;
+            label8.Text = "Symptoms";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label9.Location = new Point(396, 105);
+            label9.Name = "label9";
+            label9.Size = new Size(46, 15);
+            label9.TabIndex = 24;
+            label9.Text = "Weight";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(538, 131);
+            label10.Name = "label10";
+            label10.Size = new Size(17, 15);
+            label10.TabIndex = 25;
+            label10.Text = "in";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(447, 131);
+            label11.Name = "label11";
+            label11.Size = new Size(17, 15);
+            label11.TabIndex = 26;
+            label11.Text = "lb";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold);
+            label12.ForeColor = Color.IndianRed;
+            label12.Location = new Point(290, 6);
+            label12.Name = "label12";
+            label12.Size = new Size(184, 30);
+            label12.TabIndex = 27;
+            label12.Text = "Routine Checkup";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label13.Location = new Point(361, 44);
+            label13.Name = "label13";
+            label13.Size = new Size(32, 15);
+            label13.TabIndex = 28;
+            label13.Text = "Date";
+            // 
+            // panel15
+            // 
+            panel15.BackColor = Color.IndianRed;
+            panel15.Location = new Point(217, 152);
+            panel15.Name = "panel15";
+            panel15.Size = new Size(50, 1);
+            panel15.TabIndex = 57;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.IndianRed;
+            panel1.Location = new Point(298, 152);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(60, 1);
+            panel1.TabIndex = 58;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.IndianRed;
+            panel2.Location = new Point(390, 153);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(60, 1);
+            panel2.TabIndex = 59;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.IndianRed;
+            panel3.Location = new Point(481, 153);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(60, 1);
+            panel3.TabIndex = 60;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.IndianRed;
+            panel4.Location = new Point(298, 201);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(60, 1);
+            panel4.TabIndex = 61;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.IndianRed;
+            panel5.Location = new Point(412, 180);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(125, 1);
+            panel5.TabIndex = 62;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.IndianRed;
+            panel6.Location = new Point(412, 280);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(125, 1);
+            panel6.TabIndex = 63;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.IndianRed;
+            panel7.Location = new Point(412, 181);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(1, 101);
+            panel7.TabIndex = 64;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.IndianRed;
+            panel8.Location = new Point(536, 179);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(1, 101);
+            panel8.TabIndex = 65;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = Color.IndianRed;
+            panel9.Location = new Point(217, 201);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(50, 1);
+            panel9.TabIndex = 59;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = Color.IndianRed;
+            panel10.Location = new Point(217, 152);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(50, 1);
+            panel10.TabIndex = 57;
+            // 
+            // panel11
+            // 
+            panel11.BackColor = Color.IndianRed;
+            panel11.Location = new Point(217, 201);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(50, 1);
+            panel11.TabIndex = 59;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(358, 131);
+            label14.Name = "label14";
+            label14.Size = new Size(18, 15);
+            label14.TabIndex = 66;
+            label14.Text = "°F";
             // 
             // ManageVisits
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(563, 305);
+            Controls.Add(label14);
+            Controls.Add(panel11);
+            Controls.Add(panel9);
+            Controls.Add(panel8);
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(panel10);
+            Controls.Add(panel15);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(heightTextBox);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(diastolicTextBox);
+            Controls.Add(appointmentIdTextBox);
+            Controls.Add(label2);
+            Controls.Add(symptomsTextBox);
+            Controls.Add(temperatureTextBox);
+            Controls.Add(weightTextBox);
+            Controls.Add(clearButton);
+            Controls.Add(bpmTextBox);
+            Controls.Add(systolicTextBox);
+            Controls.Add(saveButton);
+            Controls.Add(visitDateTimePicker);
             Controls.Add(label1);
+            Controls.Add(visitsListBox);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2);
             Name = "ManageVisits";
             Text = "ManageVisits";
             ResumeLayout(false);
@@ -58,5 +488,42 @@
         #endregion
 
         private Label label1;
+        private ListBox visitsListBox;
+        private DateTimePicker visitDateTimePicker;
+        private Button saveButton;
+        private TextBox systolicTextBox;
+        private TextBox bpmTextBox;
+        private Button clearButton;
+        private TextBox weightTextBox;
+        private TextBox temperatureTextBox;
+        private TextBox symptomsTextBox;
+        private Label label2;
+        private TextBox appointmentIdTextBox;
+        private TextBox diastolicTextBox;
+        private Label label3;
+        private Label label4;
+        private TextBox heightTextBox;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Panel panel15;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private Panel panel9;
+        private Panel panel10;
+        private Panel panel11;
+        private Label label14;
     }
 }
